919100733823be7d0d46d651655c3435b27122f5
Leeah Mahon <leeahmahon@149-162-193-137.dhcp-in.iupui.edu>
2019-06-04 16:54:26 -0400

    updated fruits

---
+++ fruitlist.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "grape", "plum"]
+if fruits[0] == "apple":
+    print("Yum!")
+elif fruits[0] == "cardboard" or fruits[0] == "sand":
+    print("Yuck!")
+else:
+    print("Not bad.")
